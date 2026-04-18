6c1b1f2074a5a3850fd5750f32e7831f56b64ec8
mpetschauerIndium <110611109+mpetschauerIndium@users.noreply.github.com>
2026-04-18 16:19:22 -0400

    Add module five API routes and trip data endpoints

---
+++ app_api/controllers/db.js
@@ -0,0 +1,27 @@
+let mongoose;
+
+try {
+  mongoose = require("mongoose");
+} catch (err) {
+  mongoose = null;
+}
+
+if (mongoose) {
+  const dbURI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/travlr";
+
+  mongoose.connect(dbURI);
+
+  mongoose.connection.on("connected", () => {
+    console.log(`Mongoose connected to ${dbURI}`);
+  });
+
+  mongoose.connection.on("error", (err) => {
+    console.log("Mongoose connection error:", err);
+  });
+
+  mongoose.connection.on("disconnected", () => {
+    console.log("Mongoose disconnected");
+  });
+}
+
+require("../models/trips");
