8b092abea92dc333aa26b54c09b24a784a52732a
mpetschauerIndium <110611109+mpetschauerIndium@users.noreply.github.com>
2026-04-26 22:16:07 -0400

    Enable CORS for Angular admin API requests

--- app.js
+++ app.js
@@ -12,6 +12,18 @@ const port = 3000;
 app.set("views", path.join(__dirname, "app_server", "views"));
 app.set("view engine", "hbs");
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept,Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
